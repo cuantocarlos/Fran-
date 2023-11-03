590f6f47e9e6af6755acce0c5b255a10684ba868
fran <fco.carlos@proton.me>
2023-11-03 16:51:52 +0100

    Formulario cambio contrasenya

--- templates/formularioPerfilUsuario.php
+++ templates/formularioPerfilUsuario.php
@@ -1,2 +1,12 @@
 <?php
 ?>
+<form action="index.php" method="post">
+    <!--Introducir contrasenya-->
+    <label for="pass">Contrasenya</label>
+    <input type="password" name="pass" id="pass" value="<?php echo $pass; ?>">
+    <br>
+    <label for="pass2">Repite Contrasenya</label>
+    <input type="password" name="pass2" id="pass2" value="<?php echo $pass2; ?>">
+    <br>
+    <input type="submit" name="cambiarPass" value="Cambiar">
+</form>
