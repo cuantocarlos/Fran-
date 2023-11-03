9b688c62f46726f6f85394ff1ba8e3c868c0e266
CSPablo <cspablo1540@gmail.com>
2023-11-03 16:32:42 +0100

    Añado pintaRadio/Check/Select a bcomponentes

---
+++ libs/bcomponentes.php
@@ -0,0 +1,25 @@
+<?php
+
+
+function pintaRadio(array $array, string $grupo){
+    for($i = 0; $i < count($array); $i++){
+        echo "<input type=\"radio\" name=\"$grupo\" value=\"$array[$i]\"> $array[$i] <br>";
+    }
+
+}
+function pintaCheck(array $array, string $grupo){
+    for($i = 0; $i < count($array); $i++){
+        echo "<input type=\"checkbox\" name=\"".$grupo."[]\" value=\"$array[$i]\"> $array[$i] <br>";
+    }
+}
+
+function pintaSelect(array $array, string $grupo){
+    echo "<select name=\"$grupo\">";
+    for($i = 0; $i < count($array); $i++){
+        echo "<option value=\"$grupo[$i]\">$grupo[$i]</option>";
+    }
+    echo "</select>";
+
+
+}
+?>
