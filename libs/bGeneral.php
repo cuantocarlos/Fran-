7b833865eced427f6aa3de4b7cd38ee5d0311227
fran <fco.carlos@proton.me>
2023-11-03 16:39:08 +0100

    Arreglado nombres

--- libs/bGeneral.php
+++ libs/bGeneral.php
@@ -1,25 +1,6 @@
 <?php
 //Pinta la cabecera HTML
-function cabecera($titulo=NULL) // el archivo actual
-{
-    if (is_null($titulo)) {
-        $titulo = basename(__FILE__);
-    }
-    ?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-<title>
-				<?php
-    echo $titulo;
-    ?>
-			
-			</title>
-<meta charset="utf-8" />
-</head>
-<body>
-<?php
-}
+
 
 //Pinta el pie de página HTML
 function pie()
