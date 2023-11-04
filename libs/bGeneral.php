677372f029f4561fef03ea75527607446a117d04
CSPablo <cspablo1540@gmail.com>
2023-11-04 12:35:36 +0100

    Añado cFile->bGeneral.php  y config.php

--- libs/bGeneral.php
+++ libs/bGeneral.php
@@ -88,3 +88,94 @@ function cEmail(string $email, string $campo, array &$errores)
     $errores[$campo] = "Error en el campo $campo";
     return false;
 }
+
+
+/**
+ * Funcion cFile
+ * 
+ * Valida la subida de un archivo a un servidor.
+ *
+ * @param string $nombre
+ * @param array $extensiones_validas
+ * @param string $directorio
+ * @param integer $max_file_size
+ * @param array $errores
+ * @param boolean $required
+ * @return boolean|string
+ */
+function cFile(string $nombre, array &$errores, array $extensionesValidas, string $directorio, int  $max_file_size,  bool $required = TRUE)
+{
+    // Caso especial que el campo de file no es requerido y no se intenta subir ningun archivo
+    if ((!$required) && $_FILES[$nombre]['error'] === 4)
+        return true;
+    // En cualquier otro caso se comprueban los errores del servidor 
+    if ($_FILES[$nombre]['error'] != 0) {
+        $errores["$nombre"] = "Error al subir el archivo " . $nombre . ". Prueba de nuevo";
+        return false;
+    } else {
+
+        $nombreArchivo = strip_tags($_FILES["$nombre"]['name']);
+        /*
+             * Guardamos nombre del fichero en el servidor
+            */
+        $directorioTemp = $_FILES["$nombre"]['tmp_name'];
+        /*
+             * Calculamos el tamaño del fichero
+            */
+        $tamanyoFile = filesize($directorioTemp);
+        
+        /*
+            * Extraemos la extensión del fichero, desde el último punto.
+            */
+        $extension = strtolower(pathinfo($nombreArchivo, PATHINFO_EXTENSION));
+
+        /*
+            * Comprobamos la extensión del archivo dentro de la lista que hemos definido al principio
+            */
+        if (!in_array($extension, $extensionesValidas)) {
+            $errores["$nombre"] = "La extensión del archivo no es válida";
+            return false;
+        }
+        /*
+            * Comprobamos el tamaño del archivo
+            */
+        if ($tamanyoFile > $max_file_size) {
+            $errores["$nombre"] = "La imagen debe de tener un tamaño inferior a $max_file_size kb";
+            return false;
+        }
+
+        // Almacenamos el archivo en ubicación definitiva si no hay errores ( al compartir array de errores TODOS LOS ARCHIVOS tienen que poder procesarse para que sea exitoso. Si cualquiera da error, NINGUNO  se procesa)
+
+        if (empty($errores)) {
+            /**
+             * Comprobamos si el directorio pasado es válido
+             */
+            if (is_dir($directorio)) {
+                /**
+             * Tenemos que buscar un nombre único para guardar el fichero de manera definitiva.
+             * Podemos hacerlo de diferentes maneras, en este caso se hace añadiendo microtime() al nombre del fichero 
+             * si ya existe un archivo guardado con ese nombre.
+             * */
+                $nombreArchivo = is_file($directorio . DIRECTORY_SEPARATOR . $nombreArchivo) ? time() . $nombreArchivo : $nombreArchivo;
+                $nombreCompleto = $directorio . DIRECTORY_SEPARATOR . $nombreArchivo;
+                /**
+                 * Movemos el fichero a la ubicación definitiva.
+                 * */
+                if (move_uploaded_file($directorioTemp, $nombreCompleto)) {
+                    /**
+                     * Si todo es correcto devuelve la ruta y nombre del fichero como se ha guardado
+                     */
+
+
+                    return $nombreCompleto;
+                } else {
+                    $errores["$nombre"] = "Ha habido un error al subir el fichero";
+                    return false;
+                }
+            }else {
+                $errores["$nombre"] = "Ha habido un error al subir el fichero";
+                return false;
+            }
+        }
+    }
+}
