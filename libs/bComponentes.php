693e95d3fa98269e88f0077d5c02ddb9b3f6d348
cuantocarlos <fco.carlos@protonmail.com>
2023-11-04 16:21:36 +0100

    mejoras en sintaxis de todo el documento, mejoras varias y mejoras en formulario perfil usuario

--- libs/bComponentes.php
+++ libs/bComponentes.php
@@ -1,25 +1,25 @@
 <?php
 
-
-function pintaRadio(array $array, string $grupo){
-    for($i = 0; $i < count($array); $i++){
+function pintaRadio(array $array, string $grupo)
+{
+    for ($i = 0; $i < count($array); $i++) {
         echo "<input type=\"radio\" name=\"$grupo\" value=\"$array[$i]\"> $array[$i] <br>";
     }
 
 }
-function pintaCheck(array $array, string $grupo){
-    for($i = 0; $i < count($array); $i++){
-        echo "<input type=\"checkbox\" name=\"".$grupo."[]\" value=\"$array[$i]\"> $array[$i] <br>";
+function pintaCheck(array $array, string $grupo)
+{
+    for ($i = 0; $i < count($array); $i++) {
+        echo "<input type=\"checkbox\" name=\"" . $grupo . "[]\" value=\"$array[$i]\"> $array[$i] <br>";
     }
 }
 
-function pintaSelect(array $array, string $grupo){
+function pintaSelect(array $array, string $grupo)
+{
     echo "<select name=\"$grupo\">";
-    for($i = 0; $i < count($array); $i++){
+    for ($i = 0; $i < count($array); $i++) {
         echo "<option value=\"$array[$i]\">$array[$i]</option>";
     }
     echo "</select>";
 
-
 }
-?>
