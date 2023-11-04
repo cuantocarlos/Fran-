b66283835443985f34114458230850f19eba649b
cuantocarlos <fco.carlos@protonmail.com>
2023-11-04 23:35:46 +0100

    arreglos en assets y añadida el pintaselec de idiomas

--- php/saniPerfilUsuario.php
+++ php/saniPerfilUsuario.php
@@ -1,7 +1,9 @@
 <?php
 include "/libs/bGeneral.php";
+include "/libs/config.php";
+include "/templates/cabecera.php";
 $errores = false;
-cabecera();
+
 
 //array donde almacenaremos el texto de los errores encontrados
 $errores = [];
@@ -17,11 +19,19 @@ if (!isset($_REQUEST['bAceptar'])) {
 
 } else { //sanitizamos
     //Si el formulario si ha aparecido, procesamos los datos
-    cPass($new_pass, );
+    cPass($new_pass );
     cFile($img_perfil);
     cTexto($desc_personal);
 
-}
+    if (empty($errores)) {
+    //Si no hay errores redireccionamos a validPerfilUsuario.php
+        header("location:validPerfilUsuario.php?new_pass=$new_pass&img_perfil=$img_perfil&idiomas=$idiomas&desc_personal=$desc_personal");
+    } else {
+    //Si hay errores volvemos a mostrar el formulario
+        include 'formPerfilUsuario.php';
+    }
 
+}
+include "/templates/pie.php";
 ?>
 <!--falta idiomas-->
