55aba0e6f1b4f14fe748e5d6ee96ae5e631b1580
Francisco Carlos Beltran Arsene <fco.carlos@protonmail.com>
2023-11-13 00:35:49 +0100

    Update validPerfilUsuario.php
    Ahora el usuario percibe los cambios modificados

--- templates/validPerfilUsuario.php
+++ templates/validPerfilUsuario.php
@@ -7,7 +7,17 @@ require "../libs/config.php";
 $new_pass = recoge("new_pass");
 $img_perfil = recoge("img_perfil");
 $idioma = recoge("idioma");
-$desc_personal = "";
+$desc_personal = recoge("desc_personal");
 
 
 ?>
+<p>Estos son los datos de tu perfil ahora:</p>
+<ul>
+    <li>Nombre: <?php echo $_SESSION["nombre"] ?></li>
+    <li>Apellidos: <?php echo $_SESSION["apellidos"] ?></li>
+    <li>Correo: <?php echo $_SESSION["correo"] ?></li>
+    <li>Contraseña: <?php echo $new_pass ?></li>
+    <li>Imagen de perfil: <?php echo $img_perfil ?></li>
+    <li>Idioma: <?php echo $idioma ?></li>
+    <li>Descripción personal: <?php echo $desc_personal ?></li>
+</ul>
