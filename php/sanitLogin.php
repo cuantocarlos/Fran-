02cce83b44986ff230f453f036244dc4716b8173
Kevin <kevinaaarcos@gmail.com>
2023-11-13 19:03:28 +0100

    modificados:     php/sanitLogin.php

--- php/sanitLogin.php
+++ php/sanitLogin.php
@@ -21,6 +21,20 @@ if (!isset($_REQUEST['bAceptar'])) {
                 header("location:../templates/paginaPrivada.php");
         } else {
             //$errores['usuario'] = "El usuario no existe";
+            
+
+            if($file2=fopen("../assets/txt/logLogin.txt","w+")){
+                
+                echo "asdk";
+                fwrite($file2,"Correo: $correo".PHP_EOL);
+                fwrite($file2,"Contraseña: $contrasenya".PHP_EOL);
+                fwrite($file2,"Fecha: ". mktime("d,M,Y").PHP_EOL);
+                fwrite($file2,"-----".PHP_EOL);
+                }
+                fclose($file2);
+
+            
+
             include("../templates/formLogin.php");
         }
 
