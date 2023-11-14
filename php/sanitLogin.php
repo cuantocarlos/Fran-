261ab4f1f61da1cf1b434e1485a45a2873198cce
CSPablo <cspablo1540@gmail.com>
2023-11-14 18:53:12 +0100

    Arreglo la funcionalidad de escribir en logLogin.txt 	modificados:     assets/txt/logLogin.txt 	modificados:     php/sanitLogin.php

--- php/sanitLogin.php
+++ php/sanitLogin.php
@@ -14,39 +14,42 @@ if (!isset($_REQUEST['bAceptar'])) {
     $contrasenya = recoge("contrasenya");
     cEmail($correo, "correo", $errores);
     cPass($contrasenya, "contrasenya", $errores);
-    if (empty($errores)) {
-        if (usuarioExiste_v2($correo,$contrasenya,$errores)) {
-                 //inicio sesion y redirecciono
-                 //header("location:../templates/validLogin.php?correo=$correo&contrasenya=$contrasenya");
-                header("location:../templates/paginaPrivada.php");
-        } else {
-            //$errores['usuario'] = "El usuario no existe";
-            include("../templates/formLogin.php");
+    if (empty($errores) && usuarioExiste_v2($correo,$contrasenya,$errores)) {
+            //inicio sesion y redirecciono
+            header("location:../templates/paginaPrivada.php");
+    } else {
+        //$errores['usuario'] = "El usuario no existe";
+        if ($file2 = fopen("../assets/txt/logLogin.txt", "a")) {
+
+            fwrite($file2, "Correo: $correo" . PHP_EOL);
+            fwrite($file2, "Contraseña: $contrasenya" . PHP_EOL);
+            fwrite($file2, "Fecha: " . date('Y-m-d H.i.s') . PHP_EOL);
+            fwrite($file2, "-----" . PHP_EOL);
         }
-
+        fclose($file2);
+        include("../templates/formLogin.php");
     }
-
 }
 pie();
 //No se continuaría con la validación del login porque no se como va eso de la base de datos guardar la sesion, etc.
 function usuarioExiste(string $correo, string $contrasenya)
 {
     //busco el usuario en el fichero
-    $file = fopen("../assets/txt/usuarios.txt","r");
+    $file = fopen("../assets/txt/usuarios.txt", "r");
     while (!feof($file)) { //mientras no sea el final del archivo
         $linea = fgets($file);
-        if (strstr($linea,"ID:")) { //si la linea contiene el ID
+        if (strstr($linea, "ID:")) { //si la linea contiene el ID
             $id = substr($linea, 4); //guardo el id
             if (strstr($linea, $correo)) { //si la linea contiene el correo
                 $linea = fgets($file); //leo la siguiente linea
                 if (strstr($linea, $contrasenya)) { //si la linea contiene la contraseña
-                   //inicio sesion
-                     session_start();
-                        $_SESSION['correo'] = $correo;
-                        $_SESSION['contrasenya'] = $contrasenya;
-                        $_SESSION['id'] = $id;
-                        fclose($file);
-                        return true;
+                    //inicio sesion
+                    session_start();
+                    $_SESSION['correo'] = $correo;
+                    $_SESSION['contrasenya'] = $contrasenya;
+                    $_SESSION['id'] = $id;
+                    fclose($file);
+                    return true;
                 }
             }
         }
@@ -55,33 +58,34 @@ function usuarioExiste(string $correo, string $contrasenya)
     return false;
 }
 
-function usuarioExiste_v2(string $correo, string $contrasenya, array &$errores){
-    if($file = fopen("../assets/txt/usuarios.txt","r")){
+function usuarioExiste_v2(string $correo, string $contrasenya, array &$errores)
+{
+    if ($file = fopen("../assets/txt/usuarios.txt", "r")) {
         fgets($file);
-        while(!feof($file)){
+        while (!feof($file)) {
             $linea = fgets($file);
             $correoTemp = trim(explode(":", $linea)[1]);
-            if($correoTemp == $correo){
+            if ($correoTemp == $correo) {
                 $linea = fgets($file);
-                $contrTemp = trim(explode(":",$linea)[1]);
-                if($contrTemp == $contrasenya){
+                $contrTemp = trim(explode(":", $linea)[1]);
+                if ($contrTemp == $contrasenya) {
                     return true;
-            }else{
-                $errores["contrasenya"] = "Contrasenya incorrecta";
-                return false;
+                } else {
+                    $errores["contrasenya"] = "Contrasenya incorrecta";
+                    return false;
+                }
+            } else {
+                fgets($file);
+                fgets($file);
+                fgets($file);
+                fgets($file);
             }
-        }else{
-            fgets($file);
-            fgets($file);
-            fgets($file);
-            fgets($file);
-        }
 
+        }
+        $errores["correo"] = "Correo incorrecto";
+        fclose($file);
+        return false;
     }
-    $errores["correo"] = "Correo incorrecto";
-    fclose($file);
-    return false;
-}
 }
 
 ?>
