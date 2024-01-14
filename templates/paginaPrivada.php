7a6e55a9685cfa6293b55c2a70bb91be5ca48970
kevinarcos <kevinarcos@abastos.org>
2024-01-14 19:04:25 +0100

    Creado modificar usuarios

--- templates/paginaPrivada.php
+++ templates/paginaPrivada.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
-include "../libs/config.php";
+    include('libs\config.php');
+    include('modelo\conexion.php');
+    include('modelo\consultas.php');
 
 //Si no tiene nivel=1, se le lleva de vuelta a la página inicial.
 
@@ -13,9 +15,19 @@ controlAcceso();
 //echo "<br>";
 //echo "<img src=\"../assets/imgUsuarios/".$_SESSION["img"]."\">";
 //echo "<h1>Servicios que puedes contratar </h1><br>";
-mostrarServicios();
-echo "<br><a href=\"../php/sanitAltaServicio.php\">Dar de alta un servicio nuevo</>";
-echo "<br><a href=\"../php/salir.php\">Salir del sistema</a>";
+
+//mostrarServicios();
+
+consultarServicios($pdo);
+?>
+
+ <br><a href="../php/sanitAltaServicio.php\">Dar de alta un servicio nuevo</>;
+
+
+ 
+ <br><a href="../php/salir.php\">Salir del sistema</a>;
+
+<?=
 pie();
 
 
